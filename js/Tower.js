b0062e497c93127ca389fdde46729d644d94bf4d
DharshithHari24 <96918607+DharshithHari24@users.noreply.github.com>
2023-09-09 19:54:25 +0530

    Update Tower.js

--- js/Tower.js
+++ js/Tower.js
@@ -1,27 +1,31 @@
 
 class Tower {
     constructor(x, y, width, height) {
+
       var options = {
-//set the static body(challenge 2)
-      
+        //set the static body(challenge 2)
+        isStatics: true
       };
-        //Load the image of the tower(challenge 5)
-this.width = width;
-    this.height = height;
+
+      //Load the image of the tower(challenge 5)
+        this.width = width;
+        this.height = height;
+
       //write the instruction to create a rectangular tower body(challenge1)
+        this.image = loadImage("assets/tower.png");
+
       //add the option inside the rectangular body(challenge 2)
+        this.body = Bodies.rectangle(200,200,50,50,options);
+
       //Add this object to the World(challenge 2)
+        World.add(world, this.body);
     }
+
 display() {
     var pos = this.body.position;
     var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    imageMode(CENTER);
-            //change the rect as image(challenge 5)
 
-//display the rect function (challenge 3)
-  pop();
+    imageMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
   }
 }
