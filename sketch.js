68e25f0366ee9237fa376e39122d97a2e968b15e
DharshithHari24 <96918607+DharshithHari24@users.noreply.github.com>
2023-09-10 20:18:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,41 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var engine, world;
+var canvas, angle, tower, ground, cannon;
 
-let engine;
-let world;
 
-var tower;
+
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.gif"); 
+  backgroundImg = loadImage("./assets/background.gif");
   towerImage = loadImage("./assets/tower.png");
 
 }
 
-
 function setup() {
   canvas = createCanvas(1200,600);
-
   engine = Engine.create();
   world = engine.world;
+  angle = -PI / 4;
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 110, 50, angle);
+  
 
-  tower = new Tower(150,350,160,310);
-
-  rectMode(CENTER);
-  ellipseMode(RADIUS);
 }
 
-function draw() 
-{
+function draw() {
   background(189);
   image(backgroundImg, 0, 0, width, height);
-  tower.display();
-  Engine.update(engine);
-}
 
+  
+
+  Engine.update(engine);
+  
+
+  cannon.display();
+  tower.display();
+  
+ 
+}
